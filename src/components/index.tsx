056207cc82d0cf0dfef5c2a4f166934b41c996e3
Rafael Lopez Martinez <rafael.lop.mar@gmail.com>
2020-10-20 16:29:09 +0200

    Add header link component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import VerticalSpacer from "./VerticalSpacer";
 import Link from "./Link";
+import HeaderLink from "./HeaderLink";
 
-export { VerticalSpacer, Link };
+export { VerticalSpacer, Link, HeaderLink };
